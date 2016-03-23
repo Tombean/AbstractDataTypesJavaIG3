f6a1465ea0fc287446cec1d00872ede7009a008e
Tombean <tom.somervilleroberts@gmail.com>
2016-03-23 16:02:39 +0100

    added constructor to undirectedEdge

--- src/graph/UndirectedEdge.java
+++ src/graph/UndirectedEdge.java
@@ -1,5 +1,13 @@
 package graph;
 
 public class UndirectedEdge implements Edge {
-
+	Vertex[] vertex;
+	public UndirectedEdge(Vertex[] vertexInit){
+		this.vertex = new Vertex()[2];
+		if (vertexInit.length() ==  2 ){
+			this.vertex = vertexInit;
+		}
+		else{ System.out.println("An edge needs 2 vertex, the amount of vertex given for this edge is wrong, the edge hasn't been initialized. ");}
+	}
 }
+
