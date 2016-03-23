16984bfe9544063be5168e261af6fe5a542a3f0a
louis <louis-rogeon@outlook.fr>
2016-03-23 13:04:17 +0100

    Add creators to directedEdge

--- src/graph/DirectedEdge.java
+++ src/graph/DirectedEdge.java
@@ -1,5 +1,11 @@
 package graph;
 
 public class DirectedEdge implements Edge {
+	Vertex pointDebut;
+	Vertex pointFin;
+	public DiectedEdge(pointDebut,pointFin) {
+		this.pointDebut= pointDebut;
+		this.pointFin= pointFin;
+	}
 	
 }
