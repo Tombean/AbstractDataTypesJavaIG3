c01858c81c0cc1211157b447ec3ec855869f0759
bmirona <b.mirona@yahoo.com>
2016-03-29 17:37:04 +0200

    Update UndirectedEdge.java
    add doc

--- src/graph/UndirectedEdge.java
+++ src/graph/UndirectedEdge.java
@@ -1,6 +1,11 @@
 package graph;
 
 public class UndirectedEdge extends Edge {
+	/**
+	 * Creates a new undirected edge, going from the source vertex to the target vertex 
+	 * @param sourceVertex
+	 * @param targetVertex
+	 */
 	public UndirectedEdge(Vertex sourceVertex, Vertex targetVertex ){
 		this.vertex = new Vertex[2];
 		this.vertex[0] = sourceVertex;
