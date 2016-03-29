1e15f6423f59c9f0340e03caa8872b90462fd2f9
Tombean <tom.somervilleroberts@gmail.com>
2016-03-29 19:57:11 +0200

    creation of TestGraph

---
+++ src/graph/TestGraph.java
@@ -0,0 +1,12 @@
+package graph;
+
+public class TestGraph  {
+
+	public static void Main( String[] argz ){
+		double coordinatesA[] = {0.0 , 0.0 };
+		double coordinatesB[] = {1.0 , 1.0 };
+		Vertex A =  new Vertex( coordinatesA );
+		Vertex B =  new Vertex( coordinatesB );
+	}
+
+}
