9a9fadc039836a190b3c72c53517dfe4731c4138
bmirona <b.mirona@yahoo.com>
2016-03-29 17:50:08 +0200

    Update Vertex.java
    add doc

--- src/graph/Vertex.java
+++ src/graph/Vertex.java
@@ -2,6 +2,11 @@ package graph;
 
 public class Vertex {
 	protected double[] coordinates;
+	
+	/**
+	 * Creates a new vertex
+	 * @param values
+	 */
 	public Vertex( double[] values ){
 		if (values.length > 0){
 			coordinates =  new double[values.length];
@@ -14,10 +19,18 @@ public class Vertex {
 		
 	}
 	
+	/**
+	 * Returns an array of coordinates of a vertex
+	 * @return an array of coordinates
+	 */
 	public double[] getCoordinates(){
 		return this.coordinates;
 	}
 	
+	/**
+	 * Modifies a vertex, by changing the coordinates
+	 * @param values, coordinates of a vertex to set
+	 */
 	public void setCoordinates( double[] values ){
 		if (values.length == coordinates.length ){
 			this.coordinates = values;
