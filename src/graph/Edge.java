5a4440f08664e3be3fbde97ebdf39f992c5e6718
bmirona <b.mirona@yahoo.com>
2016-03-29 17:13:48 +0200

    Update Edge.java
    Add doc

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -3,10 +3,18 @@ package graph;
 public abstract class Edge {
 	protected Vertex[] vertex;
 	
+	/**
+	 * Returns the vertex formed by 2 coordinates of an edge
+	 * @return a vertex
+	 */
 	public Vertex[] getEnds(){
 		return this.vertex;
 	}
 	
+	/**
+	 * Checks if the vertex can be created
+	 * @param v: array of Vertex
+	 */
 	public void setEnds( Vertex[] v){
 		if (v.length ==  2 ){
 			this.vertex = v;
